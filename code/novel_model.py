f9efc4a58d878c18cd021fa23816d03d67535727
Fulton <fultonwang@gmail.com>
2018-04-15 16:51:06 -0400

    novel model added

--- code/novel_model.py
+++ code/novel_model.py
@@ -27,27 +27,43 @@ def create_new_conv_layer(input_data, num_input_channels, num_filters, filter_sh
     return out_layer, weights
 
 def get_novel_model(seq_batch, label_batch, seq_len, label_len, infos):
-    # infos is tuples of num_filters (i.e. # of motifs), filter_shape (i.e. ~4 x 20), pool_shape (i.e. 2 x 2)
+    # infos is tuples of num_filters (i.e. # of motifs), filter_shape (i.e. ~4 x 20), pool_shape (i.e. 2 x 2), num_input_channels
     
     NUM = 4
-    num_input_channels = 1
     
-    reshaped_seq_batch = tf.reshape(seq_batch, (-1, NUM, seq_len/NUM, num_input_channels))
+    reshaped_seq_batch = tf.reshape(seq_batch, (-1, 1, seq_len/NUM, NUM))
 
+#    reshaped_seq_batch = tf.reshape(seq_batch, (-1, seq_len/NUM, NUM))
+#    reshaped_seq_batch = tf.expand_dims(reshaped_seq_batch, 1)
+    
     layer = reshaped_seq_batch
-    for (layer_idx, (num_filters, filter_shape, pool_shape)) in enumerate(infos):
+    for (layer_idx, (num_filters, filter_shape, pool_shape, num_input_channels)) in enumerate(infos):
         layer, weights = create_new_conv_layer(layer, num_input_channels, num_filters, filter_shape, pool_shape, 'layer_%d' % layer_idx)
         if layer_idx == 0:
             input_weights = weights
 
-    layer_shape = tf.slice(tf.shape(layer), [1], [3])
-    flattened_len = tf.reduce_prod(layer_shape)
-    flattened_layer = tf.reshape(layer, [-1,flattened_len])
+#    layer_shape = tf.slice(tf.shape(layer), [1], [3])
+#    flattened_len = tf.reduce_prod(layer_shape)
+#    flattened_layer = tf.reshape(layer, tf.stack([tf.constant(-1),flattened_len]))
+    
+#    flattened_layer = tf.reshape(layer, [-1,flattened_len])
 
+    print seq_len, 'h'
+    flattened_len = ((seq_len / NUM) / (2**len(infos))) * infos[-1][0]
+    flattened_layer = tf.reshape(layer, [-1, flattened_len])
+    
+#    print 'asdf', flattened_len
+#    print 'gg', tf.stack([flattened_len, tf.constant(label_len)])
+#    B = tf.Variable(tf.random_normal(tf.stack([flattened_len, tf.constant(label_len)])), dtype=tf.float32)
+#    b = tf.Variable(tf.random_normal(tf.stack([tf.constant(1), tf.constant(label_len)])), dtype=tf.float32)
+    
+    #B = tf.get_variable('B', tf.stack([flattened_len, tf.constant(label_len)]), dtype=tf.float32)
+    #b = tf.get_variable('b', tf.stack([tf.constant(1), tf.constant(label_len)]), dtype=tf.float32)
+    
     B = tf.get_variable('B', [flattened_len, label_len], dtype=tf.float32)
     b = tf.get_variable('b', [1, label_len], dtype=tf.float32)
 
-    logits = tf.matmul(seq_batch, B)
+    logits = tf.matmul(flattened_layer, B)
     logits = logits + b
 
     loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=label_batch, logits=logits), axis=[0,1])
