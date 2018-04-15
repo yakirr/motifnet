345511f8fe11c3fd9defc1d368561cb736ba437f
Reshef, Yakir <yakir@seas.harvard.edu>
2018-04-15 15:05:23 -0400

    finalized format_data

--- code/fxns.py
+++ code/fxns.py
@@ -3,70 +3,50 @@ import pandas as pd
 import collections
 import os.path, pdb
 import tensorflow as tf
+import glob
+from os.path import join
 
 def create_shards(path, train_out_path, valid_out_path, test_out_path,
         test_chrs, valid_chrs,
         blocksize=1000, text=True, binary=True, num_blocks=None, task_file='alltasks.tasknames'):
-    # output 2 folders, with samples from 
 
-    from os import listdir
-    from os.path import isfile, join
-    files = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
+    files = glob.glob(path + '/alltasks.*.examples')
 
     # figure out one hot encoding mapping
-    import string
-#    tf_d = {}
     train_chr_to_files = collections.defaultdict(list)
     valid_chr_to_files = collections.defaultdict(list)
     test_chr_to_files = collections.defaultdict(list)
     for f in files:
         _, name = os.path.split(f)
-        if name != task_file:
-            _, chr, _ = string.split(name, sep='.')
-            chr = int(chr)
-#            tf_d[tft] = 1
-            if chr in test_chrs:
-                test_chr_to_files[chr] = f
-            elif chr in valid_chrs:
-                valid_chr_to_files[chr] = f
-            else:
-                train_chr_to_files[chr] = f
+        _, chr, _ = name.split('.')
+        chr = int(chr)
+        if chr in test_chrs:
+            test_chr_to_files[chr] = f
+        elif chr in valid_chrs:
+            valid_chr_to_files[chr] = f
+        else:
+            train_chr_to_files[chr] = f
 
     tf_d = pd.Series.from_csv(join(path,task_file), index_col=None).values
 
     tf_to_pos = dict(map(reversed,enumerate(tf_d)))
 
-    
-    # create iterator over lines
-
+    # iterator over lines
     def file_input(files):
-        fs = map(open, files) 
-        while len(fs) > 0:
-            i = np.random.randint(0,len(fs))
-            try:
-                line = fs[i].next()
-                yield line
-            except StopIteration:
-                fs.pop(i)
+        for f in files:
+            with open(f,'r') as fin:
+               for line in fin:
+                   yield line
 
-            
-
-#            for f in files:
-#            fs.append(f)
-#            with open(f,'r') as fin:
-#                for line in fin:
-#                    yield line
-
-    train_readers = {chr:open(train_chr_to_files[chr]) for chr in train_chr_to_files.keys()}
-    valid_readers = {chr:open(valid_chr_to_files[chr]) for chr in valid_chr_to_files.keys()}
-    test_readers = {chr:open(test_chr_to_files[chr]) for chr in test_chr_to_files.keys()}
+    train_readers = {chr:open(f) for chr, f in train_chr_to_files.items()}
+    valid_readers = {chr:open(f) for chr, f in valid_chr_to_files.items()}
+    test_readers = {chr:open(f) for chr, f in test_chr_to_files.items()}
 
     seq_len = None
     label_len = None
 
     # read lines from iterators and write, removing readers if they are empty
     def write(readers, blocksize, path):
-
         nuc_to_onehot_d = {
             'A':[1,0,0,0],
             'G':[0,1,0,0],
@@ -97,20 +77,21 @@ def create_shards(path, train_out_path, valid_out_path, test_out_path,
             if binary_writer is None:
                 if binary:
                     binary_writer = tf.python_io.TFRecordWriter('%s/shard_%d.tfrecords' % (path, idx))
+
             key = np.random.choice(readers.keys())
             try:
                 line = readers[key].next()
             except StopIteration:
                 del readers[key]
             else:
-                tft, seq = string.split(line.strip(), sep='\t')
+                tft, seq = line.strip().split('\t')
                 seq_onehot = np.array([nuc_to_onehot(c) for c in seq]).reshape(-1)
                 seq_len = len(seq_onehot)
                 tf_onehot = np.zeros(len(tf_to_pos), dtype=int)
                 tf_onehot[tf_to_pos[tft]] = 1
                 label_len = len(tf_onehot)
                 if text:
-                    text_writer.write(string.join(map(str, seq_onehot) + map(str, tf_onehot), sep=' ') + '\n')
+                    text_writer.write(' '.join(map(str, seq_onehot) + map(str, tf_onehot)) + '\n')
                 if binary:
                     example = tf.train.Example(
                         features=tf.train.Features(
@@ -137,8 +118,11 @@ def create_shards(path, train_out_path, valid_out_path, test_out_path,
 
         pd.Series({'tf_to_pos':tf_to_pos, 'seq_len':seq_len, 'label_len':label_len}).to_csv('%s/info' % path)
 
+    print('creating training set')
     write(train_readers, blocksize, train_out_path)
+    print('creating validation set')
     write(valid_readers, blocksize, valid_out_path)
+    print('creating test set')
     write(test_readers, blocksize, test_out_path)
 
 def get_seq_and_label(out_path):
